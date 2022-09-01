c5435bf3f3f4dee9bc8ab2ee77583e6a110ed715
Nikola Georgiev <georgiev.nikito@gmail.com>
2022-09-01 21:18:08 +0300

    First attempt: too slow -> indexing

--- python/solution.py
+++ python/solution.py
@@ -14,9 +14,35 @@ from utils.image import (
 
 from utils.function_tracer import FunctionTracer
 
+def fix_image(image: StrideImage):
+    """
+    Levaraging the fact that all patterns are 5x5 squares and are not intersecting or partial
+    And the fact that we don't have pixels with values above 200 that do not belong to the pattern
+    We can scan the image to find the upper left pixel of each pattern occurance and then just 
+    subtract 150 from each pixel with value above 200 in 5x5 block
+    """
+    res = image.resolution
+
+    def to_image_idx(x, y):
+        return y * res.width + x
+
+    for y in range(res.height):
+        for x in range(res.width):
+            idx = to_image_idx(x, y)
+            pix = image.pixels_red[idx]
+            if pix >= 200:
+                for py in range(5):
+                    for px in range(5):
+                        tmp_idx = to_image_idx(x + px, y + py)
+                        tmp = image.pixels_red[tmp_idx]
+                        if tmp >= 200:
+                            image.pixels_red[tmp_idx] = tmp - 150
+
 def compute_solution(images: List[Union[PackedImage, StrideImage]]):
     ft = FunctionTracer("compute_solution", "seconds")
 
-    #TODO fill solution
+    for img in images:
+        fix_image(img)
+
     del ft
             
